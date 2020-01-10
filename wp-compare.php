58e74a317d0dd8346997cb111fdf18f1aa3dab72
Mike Miler <mike@miler.de>
2020-01-10 20:58:02 +0800

    Update version

--- wp-compare.php
+++ wp-compare.php
@@ -16,7 +16,7 @@
  * Plugin Name:       WP Compare
  * Plugin URI:        wp-mike.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           0.1.4
+ * Version:           0.1.5
  * Author:            Mike Miler
  * Author URI:        wp-mike.com
  * License:           GPL-2.0+
