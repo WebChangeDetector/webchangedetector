8501e7737d072b82b8f31a3fff87e0b5036a1aa5
repat <repat@repat.de>
2020-06-26 23:41:08 +1200

    Fixy fixy

--- admin/class-webchangedetector-admin.php
+++ admin/class-webchangedetector-admin.php
@@ -422,8 +422,8 @@ class WebChangeDetector_Admin
 
                 // Select all from same device
                 echo '<tr style="background: none; text-align: center">
-                            <td><input type="checkbox" id="select-desktop-' . $post_type . '" onclick="mmToggle( this, \'' . $post_type . '\', \'desktop\', \'' . $group_id . '\' )" /></td>
-                            <td><input type="checkbox" id="select-mobile-' . $post_type . '" onclick="mmToggle( this, \'' . $post_type . '\', \'mobile\', \'' . $group_id . '\' )" /></td>
+                            <td><input type="checkbox" id="select-desktop-' . $post_type . '" onclick="mmToggle( this, \'' . $post_type . '\', \'desktop\', \'' . $groups_and_urls['id'] . '\' )" /></td>
+                            <td><input type="checkbox" id="select-mobile-' . $post_type . '" onclick="mmToggle( this, \'' . $post_type . '\', \'mobile\', \'' . $groups_and_urls['id'] . '\' )" /></td>
                             <td></td>
                             <td></td>
                         </tr>';
@@ -463,19 +463,19 @@ class WebChangeDetector_Admin
                         }
                     }
 
-                    echo '<tr class="post_id_' . $group_id . '" id="' . $url_id . '" >';
+                    echo '<tr class="post_id_' . $groups_and_urls['id'] . '" id="' . $url_id . '" >';
                     echo '<input type="hidden" name="post_id-' . $url_id . '" value="' . $post->ID . '">';
                     echo '<input type="hidden" name="url_id-' . $url_id . '" value="' . $url_id . '">';
                     echo '<input type="hidden" name="active-' . $url_id . ' value="1">';
 
                     echo '<td class="checkbox-desktop-' . $post_type . '" style="text-align: center;">
                             <input type="hidden" value="0" name="desktop-' . $url_id . '">
-                            <input type="checkbox" name="desktop-' . $url_id . '" value="1" ' . $checked['desktop'] . ' 
+                            <input type="checkbox" name="desktop-' . $url_id . '" value="1" ' . $checked['desktop'] . '
                             id="desktop-' . $url_id . '" onclick="mmMarkRows(\'' . $url_id . '\')" ></td>';
 
                     echo '<td class="checkbox-mobile-' . $post_type . '" style="text-align: center;">
                             <input type="hidden" value="0" name="mobile-' . $url_id . '">
-                            <input type="checkbox" name="mobile-' . $url_id . '" value="1" ' . $checked['mobile'] . ' 
+                            <input type="checkbox" name="mobile-' . $url_id . '" value="1" ' . $checked['mobile'] . '
                             id="mobile-' . $url_id . '" onclick="mmMarkRows(\'' . $url_id . '\')" ></td>';
 
                     echo '<td style="text-align: left;">' . $post->post_title . '</td>';
