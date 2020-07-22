aeac3a134974f27fa2de856b2a664534e70211d8
Mike Miler <mike@miler.de>
2020-07-23 00:15:53 +0800

    Hide account data if limits are enabled

--- admin/partials/templates/account.php
+++ admin/partials/templates/account.php
@@ -1,13 +1,17 @@
 <h2>
     <span class="dashicons dashicons-admin-users"></span>
     Your Account
-    <a class="button account"
-        href="<?= WCD_APP_DOMAIN . '/account/upgrade/?type=package&id=' . $account_details['whmcs_service_id'] ?>"
-        target="_blank">
-        Upgrade
-    </a>
+    <?php if( !$website_details['enable_limits']) { ?>
+        <a class="button account"
+            href="<?= WCD_APP_DOMAIN . '/account/upgrade/?type=package&id=' . $account_details['whmcs_service_id'] ?>"
+            target="_blank">
+            Upgrade
+        </a>
+    <?php } ?>
 </h2>
-Your plan: <strong><?= $account_details['plan']['name'] ?></strong><br>
+<?php if( !$website_details['enable_limits']) { ?>
+    Your plan: <strong><?= $account_details['plan']['name'] ?></strong><br>
+<?php } ?>
 Used credits: <strong><?= $comp_usage ?> / <?= $limit ?></strong><br>
 Next renew: <strong><?= date('d/m/Y', $renew_date) ?></strong>
 <p>
