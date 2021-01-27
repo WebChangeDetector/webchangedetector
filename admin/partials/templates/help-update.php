79adc1d04a89aaaece83966e97447838cea7a6da
Mike Miler <mike@miler.de>
2021-01-27 20:36:05 +0800

    help text update detection

--- admin/partials/templates/help-update.php
+++ admin/partials/templates/help-update.php
@@ -1,23 +1,30 @@
 <?php include 'help-headline.php'; ?>
     <p>
-        Compare your website before and after updates and see the exact difference between the versions.
+        Compare your website by taking and comparing screenshots before and after updates.
+        You can see the exact difference between the screenshots in the Change Detections.
     </p>
 
-    <h3></span>Step by step</h3>
+    <h3>Step by step</h3>
+    <p>The wizard takes you step by step through the process:</p>
     <ol>
         <li>
-            Select the device(s) for the URLs you want to start the Change detection.
+            Select the URLs and screensizes (desktop and / or mobile) you want to check before and after the updates.
         </li>
         <li>
-            Click "Create reference screenshot" to create your reference image.
-        </li>
-
-        <li>
-            After updating your website, click "Create change detections" to take another screenshot and compare
-            with your reference.
+            Create the pre-update screenshots. While the screenshots are in progress, you can leave the page and come
+            back later.
         </li>
         <li>
-            Check your Change Detections at "Change Detections".
+            Make updates or other changes on your website.
         </li>
-
+        <li>
+            Create the post-update screenshots and compare them to the pre-update screenshots to see differences.
+        </li>
+        <li>
+            Check the Change Detections for differences between the pre- and post-update screenshots.
+        </li>
+        <li>
+            If something broke during the update, you see and fix it now. To verify the fix, you can start the
+            Change Detection again. The new post-update screenshots will be compared against the pre-update screenshots.
+        </li>
     </ol>
