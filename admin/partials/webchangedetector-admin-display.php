2337aa13fafdcf5951ad5e260cf967fd5d1bd472
Mike Miler <mike@miler.de>
2024-11-02 11:53:12 +0800

    switch updater to prod

--- admin/partials/webchangedetector-admin-display.php
+++ admin/partials/webchangedetector-admin-display.php
@@ -25,9 +25,9 @@ if ( ! function_exists( 'wcd_webchangedetector_init' ) ) {
 				'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
 				'proper_folder_name' => 'plugin-name', // this is the name of the folder your plugin lives in
 				'api_url' => 'https://api.github.com/repos/WebChangeDetector/wp-webchangedetector', // the GitHub API url of your GitHub repo
-				'raw_url' => 'https://raw.github.com/WebChangeDetector/wp-webchangedetector/dev', // the GitHub raw url of your GitHub repo
+				'raw_url' => 'https://github.com/WebChangeDetector/wp-webchangedetector/master', // the GitHub raw url of your GitHub repo
 				'github_url' => 'https://github.com/WebChangeDetector/wp-webchangedetector', // the GitHub url of your GitHub repo
-				'zip_url' => 'https://github.com/WebChangeDetector/wp-webchangedetector/zipball/dev', // the zip url of the GitHub repo
+				'zip_url' => 'https://github.com/WebChangeDetector/wp-webchangedetector/zipball/master', // the zip url of the GitHub repo
 				'sslverify' => true, // whether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
 				'requires' => '3.0', // which version of WordPress does your plugin require?
 				'tested' => '3.3', // which version of WordPress is your plugin tested up to?
