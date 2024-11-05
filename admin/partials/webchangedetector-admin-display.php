a719a2972ad3b27e94eb033c9c7ab811f1fae1f9
Mike Miler <mike@miler.de>
2024-11-06 07:44:49 +0800

    fix auto update settings

--- admin/partials/webchangedetector-admin-display.php
+++ admin/partials/webchangedetector-admin-display.php
@@ -213,13 +213,6 @@ if ( ! function_exists( 'wcd_webchangedetector_init' ) ) {
 			update_option( WCD_ALLOWANCES, $wcd->website_details['allowances'] );
 		}
 
-		// Moving local auto update settings to the api.
-		/*$local_auto_update_settings = get_option( WCD_AUTO_UPDATE_SETTINGS );
-		if ( $local_auto_update_settings && empty($wcd->website_details['auto_update_settings'] ) ) {
-			$wcd->update_website_details( ['auto_update_settings' => $local_auto_update_settings] );
-			delete_option( WCD_AUTO_UPDATE_SETTINGS );
-		}*/
-
 		// Update groups in case we have group ids from previous account. We need them for auto updates.
 		$groups = array(
 			'auto_detection_group'   => $wcd->website_details['auto_detection_group']['uuid'] ?? false,
