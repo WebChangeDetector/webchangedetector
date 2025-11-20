d056c0955ade7f78d50ad9dbbc9a24aa22798f64
Mike Miler <mike@miler.de>
2025-11-20 19:13:31 +0100

    fix text

--- admin/partials/templates/update-settings.php
+++ admin/partials/templates/update-settings.php
@@ -84,7 +84,7 @@ if ( ! empty( $this->admin->website_details['allowances']['manual_checks_setting
 					$current_time     = current_time( 'H:i' );
 					// translators: %1$s is the timezone display string, %2$s is the current website time.
 					$description = sprintf(
-						__( 'Times are displayed in your website timezone: %1$s | Current website time: %2$s<br><strong>Note:</strong> After daylight saving time changes, please review and re-save these settings to ensure correct timing.', 'webchangedetector' ),
+						__( 'Times are displayed in your website timezone: %1$s | Current website time: %2$s', 'webchangedetector' ),
 						$timezone_display,
 						$current_time
 					);
