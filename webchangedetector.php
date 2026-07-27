1153a933afab1e4a210cc118a270cb1359b2c89f
Mike Miler <mike@miler.de>
2026-07-27 15:46:52 -0700

    Release v4.4.0-beta.4

--- webchangedetector.php
+++ webchangedetector.php
@@ -17,7 +17,7 @@
  * Text Domain:       webchangedetector
  * Plugin URI:        https://www.webchangedetector.com
  * Description:       Detect changes on your website visually before and after updating your website. You can also run automatic checks and get notified on changes of your website.
- * Version:           4.4.0-beta.3
+ * Version:           4.4.0-beta.4
  * Requires at least: 5.5
  * Requires PHP:      7.4
  * GitHub Plugin URI: https://github.com/WebChangeDetector/webchangedetector
